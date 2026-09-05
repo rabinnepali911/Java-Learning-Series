b9826f1b1729f174c714018bd7639e09d4be61b4
rabinnepaali911 <githubrabin@gmail.com>
2026-09-05 09:37:26 +0545

    initial commit

---
+++ Intro.java
@@ -0,0 +1,14 @@
+public class Intro {
+    // this is a single line comment.
+    /*
+     this
+    is
+    the
+    multiline
+    comment
+    */
+    public static void main(String[] args){
+        System.out.println("hello world.");
+
+    }
+}
---
+++ Variable.java
@@ -0,0 +1,20 @@
+public class Variable {
+    public static void main(String[] args){
+        int enrollYear = 2081;
+        String name = "Rabin Nepali";
+        int examRollNo = 81020235;
+        String semester = "4TH";
+        String collageName = "CCT";
+        System.out.println(name + "The student of\t\t\t "+ collageName +"\t\tEnrolled in\t\t "+enrollYear +"\t\tWhose Exam role number is: \t\t" + examRollNo + "\t\tcurrently in\t\t " +semester);
+        double gpa = 3.26;
+        System.out.println(gpa);
+        char grade = 'A';
+        System.out.println(grade);
+        boolean isStudent = true;
+        System.out.println(isStudent);
+        boolean isOnline = false;
+        System.out.println(isOnline);
+
+
+    }
+}
